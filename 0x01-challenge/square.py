c9ac3bd990508891cf24ce0988d5d2a363f59311
zakaria <grarizakaria@gmail.com>
2024-02-09 15:30:06 +0100

    alx

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ """
 
 
-class square():
+class Square():
     """ """
     width = 0
     height = 0
@@ -28,7 +28,7 @@ class square():
 if __name__ == "__main__":
     """ """
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
