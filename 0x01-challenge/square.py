508365eb5f48f931a9a59d40f5fd6c7ba788a0fa
zakaria <grarizakaria@gmail.com>
2024-02-09 15:15:22 +0100

    alx

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -12,13 +12,13 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.width * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
